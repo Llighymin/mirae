36a621615b4d0a998a1e360a149e2a0107b18778
hymin1101-cyber <llig.owner@gmail.com>
2026-03-18 12:32:30 +0900

    chore: update gitignore, next.config, package.json; add vercel.json

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true  // Static Export 필수, Vercel 이미지 최적화 대신
+  },
+  compress: true,
+  generateEtags: true,
+  poweredByHeader: false
+}
 
 module.exports = nextConfig;
